16964f8a3f0762f1f0992d6f9f82f1313b2bc4c9
PiaoDongHao <lcsc294459608@gmail.com>
2022-02-09 22:29:32 +0800

    1

--- app/src/main/java/com/fyp/face/Lables.java
+++ app/src/main/java/com/fyp/face/Lables.java
@@ -1,7 +1,119 @@
 package com.fyp.face;
 
+import org.bytedeco.opencv.presets.opencv_core;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.StringTokenizer;
+
 public class Lables {
-    public Lables(String mPath) {
+
+    String mPath;
+    ArrayList<label> theList = new ArrayList<>();
+
+    class label{
+        int num;
+        String theLabel;
+
+        public label(String s, int n){
+            theLabel = s;
+            num = n;
+        }
     }
-    //TODO
+
+    public Lables(String mPath) {
+        this.mPath = mPath;
+    }
+
+    public boolean isEmpty(){
+        return theList.isEmpty();
+    }
+
+    //TODO optimize the code
+    public String get(int i) {
+        Iterator<label> it = theList.iterator();
+        while(it.hasNext()){
+            label l = it.next();
+            if(l.num == i){
+                return l.theLabel;
+            }
+        }
+        return null;
+    }
+
+    public int get(String s){
+        for(label l : theList){
+            if(l.theLabel.equalsIgnoreCase(s)){
+                return l.num;
+            }
+        }
+
+        return -1;
+    }
+
+    public void add(String s, int n){
+        theList.add(new label(s, n));
+    }
+
+    //TODO change the store function to database
+    public void Save(){
+        try{
+            File f = new File(mPath + "faces.txt");
+            f.createNewFile();
+            BufferedWriter bw = new BufferedWriter(new FileWriter(f));
+            Iterator<label> it = theList.iterator();
+            while (it.hasNext()){
+                label l = it.next();
+                bw.write(l.theLabel + "," + l.num);
+                bw.newLine();
+            }
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //TODO change the read function to database
+    public void Read(){
+        try{
+            FileInputStream fStream = new FileInputStream(mPath + "faces.txt");
+            BufferedReader br = new BufferedReader(new InputStreamReader(fStream));
+
+            String strLine;
+            theList = new ArrayList<>();
+
+            //Read File line by line
+            while((strLine = br.readLine()) != null){
+                StringTokenizer tokens = new StringTokenizer(strLine, ",");
+                String s = tokens.nextToken();
+                String sn = tokens.nextToken();
+
+                theList.add(new label(s, Integer.parseInt(sn)));
+            }
+
+            br.close();
+            fStream.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public int max(){
+        int m = 0;
+        Iterator<label> it = theList.iterator();
+        while (it.hasNext()){
+            label l = it.next();
+            if(l.num > m){
+                m = l.num;
+            }
+        }
+        return m;
+    }
 }
--- app/src/main/java/com/fyp/face/PersonRecognizer.java
+++ app/src/main/java/com/fyp/face/PersonRecognizer.java
@@ -4,27 +4,36 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.util.Log;
 
+import org.bytedeco.javacv.FrameFilter;
 import org.bytedeco.opencv.opencv_core.IplImage;
 import org.bytedeco.opencv.opencv_core.MatVector;
 import org.opencv.android.Utils;
 import org.opencv.core.Mat;
 import org.opencv.face.FaceRecognizer;
 import org.opencv.face.LBPHFaceRecognizer;
+import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.FaceRecognizerSF;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FilenameFilter;
 import java.security.spec.ECField;
+import java.util.List;
+import java.util.Vector;
 
+import static org.bytedeco.opencv.global.opencv_core.IPL_DEPTH_8U;
+import static org.bytedeco.opencv.global.opencv_core.doubleRand;
+import static org.bytedeco.opencv.global.opencv_imgproc.COLOR_BGR2GRAY;
+import static org.bytedeco.opencv.global.opencv_imgproc.cvCvtColor;
 import static org.bytedeco.opencv.helper.opencv_imgcodecs.cvLoadImage;
 
 public class PersonRecognizer {
 
-    private LBPHFaceRecognizer LBPHfr;
+    private FaceRecognizer faceRecognizer;
     String mPath;
     int count = 0;
-    Lables lablesFile;
+    Lables labelsFile;
 
     static final int WIDTH = 128;
     static final int HEIGHT = 128;
@@ -33,15 +42,16 @@ public class PersonRecognizer {
     public PersonRecognizer(String path){
 
         //Initiate LBPH face recognizer
-        LBPHfr = LBPHFaceRecognizer.create(2,8,8,8,200);
+        faceRecognizer = LBPHFaceRecognizer.create(2,8,8,8,200);
 
         //The save path
         mPath = path;
 
         //create lable files
-        lablesFile = new Lables(mPath);
+        labelsFile = new Lables(mPath);
     }
 
+    //store the photo
     void add(Mat m, String description){
 
         //create bitmap
@@ -80,9 +90,9 @@ public class PersonRecognizer {
 
         File[] imagesFiles = root.listFiles(pngFilter);
 
-        MatVector images = new MatVector(imagesFiles.length);
+        MatVector images = new MatVector();
 
-        int[] lables = new int[imagesFiles.length];
+        int[] labels = new int[imagesFiles.length];
 
         int counter = 0;
         int label;
@@ -91,13 +101,12 @@ public class PersonRecognizer {
         IplImage grayImg;
 
         int i1 = mPath.length();
-
         for(File image : imagesFiles){
             String p = image.getAbsolutePath();
             img = cvLoadImage(p);
 
             if(img==null){
-                Log.e("Error", "Eroor cvLoadImage");
+                Log.e("Error", "Error cvLoadImage");
             }
 
             int i2 = p.lastIndexOf("-");
@@ -110,9 +119,105 @@ public class PersonRecognizer {
             //get description included in file name
             String description = p.substring(i1,i2);
 
+            if(labelsFile.get(description) < 0){
+                labelsFile.add(description, labelsFile.max() + 1);
+            }
 
+            label = labelsFile.get(description);
+
+            grayImg = IplImage.create(img.width(), img.height(), IPL_DEPTH_8U, 1);
+
+            cvCvtColor(img,grayImg, Imgproc.COLOR_BGR2GRAY);
+
+            images.put(grayImg);
+
+            labels[counter] = label;
+            
+            counter++;
+        }
+        
+        if(counter > 0){
+            if(labelsFile.max() > 1){
+                faceRecognizer.train(images, labels);
+                
+            }
+            labelsFile.Save();
         }
 
         return true;
+    }
+
+    public boolean canPredict(){
+        if(labelsFile.max() > 1)
+            return true;
+        else
+            return false;
+    }
+
+    public String predict(Mat m){
+        //cannot predict
+        if(!canPredict())
+            return null;
+
+        //predict
+        int n[] = new int[1];
+        double p[] = new double[1];
+        IplImage ipl = MatToIplImage(m, WIDTH, HEIGHT);
+
+        faceRecognizer.predict(ipl, n, p);
+
+        if(n[0] != -1)
+            mProb = (int)p[0];
+        else
+            mProb = -1;
+
+        if(n[0] != -1)
+            return labelsFile.get(n[0]);
+        else
+            return "Unknown";
+    }
+
+    IplImage MatToIplImage(Mat m, int width, int height){
+        Bitmap bmp = Bitmap.createBitmap(m.width(), m.height(), Bitmap.Config.ARGB_8888);
+        Utils.matToBitmap(m, bmp);
+        return BitmapToIplImage(bmp, width, height);
+    }
+
+    IplImage BitmapToIplImage(Bitmap bmp, int width, int height){
+        if((width != -1) || (height != -1)){
+            Bitmap bmp2 = Bitmap.createScaledBitmap(bmp, width, height, false);
+            bmp = bmp2;
+        }
+
+        IplImage image = IplImage.create(bmp.getWidth(), bmp.getHeight(),IPL_DEPTH_8U, 4);
+
+        bmp.copyPixelsToBuffer(image.getByteBuffer());
+
+        IplImage grayImg = IplImage.create(image.width(), image.height(), IPL_DEPTH_8U, 1);
+
+        cvCvtColor(image, grayImg, COLOR_BGR2GRAY);
+
+        return grayImg;
+    }
+
+    protected void SaveBmp(Bitmap bmp, String path){
+        FileOutputStream file;
+        try{
+            file = new FileOutputStream(path, true);
+
+            bmp.compress(Bitmap.CompressFormat.JPEG, 100, file);
+            file.close();
+        } catch (Exception e) {
+            Log.e("error", e.getMessage() + e.getCause());
+            e.printStackTrace();
+        }
+    }
+
+    public void load(){
+        train();
+    }
+
+    public int getProb(){
+        return mProb;
     }
 }
