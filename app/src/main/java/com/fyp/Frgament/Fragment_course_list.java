e72db417736eb1e0bdab7acd5ded62c1610696e0
PiaoDongHao <lcsc294459608@gmail.com>
2022-03-11 13:18:13 +0800

    1

--- app/src/main/java/com/fyp/Frgament/Fragment_course_list.java
+++ app/src/main/java/com/fyp/Frgament/Fragment_course_list.java
@@ -6,21 +6,30 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
+import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.fyp.R;
+import com.github.chengang.library.TickView;
 import com.ramotion.foldingcell.FoldingCell;
+
+import java.sql.Time;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link Fragment_course_list#newInstance} factory method to
  * create an instance of this fragment.
  */
+
 public class Fragment_course_list extends Fragment {
 
     private static final String ARG_PARAM1 = "param1";
@@ -96,6 +105,9 @@ public class Fragment_course_list extends Fragment {
 
     private class CardItemAdapter extends ArrayAdapter<CardItem>{
 
+        static final int HANDLE_MSG_TOGGLE = 1;
+        static final int TOGGLE_DELAY_TIME = 3000;
+
         private int resourceID;
 
         public CardItemAdapter(@NonNull Context context, int resource) {
@@ -117,6 +129,8 @@ public class Fragment_course_list extends Fragment {
                 viewHolder = new ViewHolder();
                 viewHolder.fc = view.findViewById(R.id.folding_cell);
                 viewHolder.tvCardItem = view.findViewById(R.id.tv_cardText);
+                viewHolder.tk = view.findViewById(R.id.tick_view);
+                viewHolder.btn_click = view.findViewById(R.id.btn_click);
                 view.setTag(viewHolder);
             }
             else{
@@ -124,8 +138,7 @@ public class Fragment_course_list extends Fragment {
                 viewHolder = (ViewHolder)view.getTag();
             }
 
-            //widget
-
+            //set widget
             viewHolder.fc.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
@@ -134,10 +147,40 @@ public class Fragment_course_list extends Fragment {
             });
 
             viewHolder.tvCardItem.setText(i.getName());
+
+            viewHolder.tk.setClickable(false);
+
+            Handler mHandler = new Handler(){
+                public void handleMessage(Message msg){
+                    switch (msg.what){
+                        case HANDLE_MSG_TOGGLE:
+                            viewHolder.tk.toggle();
+                    }
+                }
+            };
+
+            viewHolder.btn_click.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Timer timer = new Timer();
+                    TimerTask task = new TimerTask() {
+                        @Override
+                        public void run() {
+                            Message msg = new Message();
+                            msg.what = HANDLE_MSG_TOGGLE;
+                            mHandler.sendMessage(msg);
+                        }
+                    };
+                    timer.schedule(task, TOGGLE_DELAY_TIME);
+                }
+            });
+
             return view;
         }
 
          class ViewHolder{
+            public Button btn_click;
+            public TickView tk;
             public FoldingCell fc;
             public TextView tvCardItem;
         }
